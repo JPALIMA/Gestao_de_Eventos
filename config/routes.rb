df4f91888a30380ff3b376c46c733b99443d1432
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-03 22:49:33 -0300

    implementando as ações como: detalhes do evento, como título, local e data.

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/new'
+  get 'events/show'
+  resources :events
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
