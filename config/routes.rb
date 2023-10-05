50e34834f618bec1b3beb01b3b5843161204e89d
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-04 21:40:45 -0300

    configurando arquivo config/routes.rb do  projeto existente e definindo as rotas para o controlador de eventos

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get 'events/new'
   get 'events/show'
   resources :events
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in
+  https://guides.rubyonrails.org/rounting.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #Defines the root path route ("/")
+  #root "articles#index"
 end
